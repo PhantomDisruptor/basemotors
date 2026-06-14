51e47da7e6578560bd2a8e535ab36e13ed037169
PhantomDisruptor <sashabigulaev@mail.ru>
2026-06-15 02:01:21 +0300

    upgrade7

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public',  
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://basemotors-api.onrender.com',
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
