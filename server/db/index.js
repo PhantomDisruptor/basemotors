e3bdac06a58c80ef6416740f85e10a213908a88d
PhantomDisruptor <sashabigulaev@mail.ru>
2026-06-14 22:30:37 +0300

    upgrade1

--- server/db/index.js
+++ server/db/index.js
@@ -11,6 +11,7 @@ const pool = new Pool({
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT || 5432,
+    ssl: { rejectUnauthorized: false } 
 });
 
 // Проверка подключения
